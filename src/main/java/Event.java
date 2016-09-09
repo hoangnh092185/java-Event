84fd59ec319eb966340d1c193a8084ee97a39598
Nhat Hoang <hoangnh092185@gmail.com>
2016-09-09 09:28:55 -0700

    create all the getsVariable and a method to calculates the event cost.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,18 +1,33 @@
 public class Event {
-  private int mPeople;
+  private int mPeoples;
   private int mWings;
-  // private int mChip;
-  // private int mBeer;
-  // private String mDecoration;
+  private int mChips;
+  private int mBeers;
+  private String mDecorations;
 
-  public Event(int people, int wings){
-    mPeople = people;
+  public Event(int peoples, int wings, int chips, int beers, String decorations){
+    mPeoples = peoples;
     mWings = wings;
+    mChips = chips;
+    mBeers = beers;
+    mDecorations = decorations;
   }
   public int getPeople(){
-    return mPeople;
+    return mPeoples;
   }
   public int getWings(){
     return mWings;
+  }
+  public int getChips(){
+    return mChips;
+  }
+  public int getBeers(){
+    return mBeers;
+  }
+  public String getDecorations(){
+    return mDecorations;
+  }
+  public boolean eventCalculator(){
+    return true;
   }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,18 +4,39 @@ import static org.junit.Assert.*;
 public class EventTest {
 
   @Test
-  public void eventCalculator_instantiatesCorrectly() {
-    Event testEvent = new Event(7, 15);
+  public void eventInput_instantiatesCorrectly() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
     assertEquals(true, testEvent instanceof Event);
   }
   @Test
-  public void eventCalculator_getsPeople_7() {
-    Event testEvent = new Event(7, 15);
+  public void eventInput_getsPeople_7() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
     assertEquals(7, testEvent.getPeople());
   }
   @Test
-  public void eventCalculator_getsChickenWings_15() {
-    Event testEvent = new Event(7, 15);
+  public void eventInput_getsChickenWings_15() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
     assertEquals(15, testEvent.getWings());
   }
+  @Test
+  public void eventInput_getsChips_4() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
+    assertEquals(4, testEvent.getChips());
+  }
+  @Test
+  public void eventInput_getsBeers_5() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
+    assertEquals(9, testEvent.getBeers());
+  }
+  @Test
+  public void eventInput_getsDecorations_Yes() {
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
+    assertEquals("Yes", testEvent.getDecorations());
+  }
+  @Test
+  public void eventCalculator_instantiatesCorrectly(){
+    Event testEvent = new Event(7, 15, 4, 9, "Yes");
+    assertEquals(false, testEvent.eventCalculator());
+  }
+
 }
